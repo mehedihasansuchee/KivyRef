7ed0f4cf208ada8a975a57429246f67a9526243c
mehedihasansuchee <145920667+mehedihasansuchee@users.noreply.github.com>
2023-10-15 21:33:59 +0600

    Image2.py

---
+++ Image2.py
@@ -0,0 +1,20 @@
+from kivy.app import App 
+from kivy.uix.boxlayout import BoxLayout
+from kivy.uix.image import Image
+
+class ImageExample(BoxLayout):
+    def __init__(self, **kwargs):
+        super(ImageExample, self).__init__(**kwargs)
+
+        #  Creat an Image widge
+        self.image = Image(source='../img/ball.jpg')
+
+        #  Add the Image widget to the layout
+        self.add_widget(self.image)
+
+class MyApp(App):
+    def build(self):
+        return ImageExample()
+    
+if __name__ == '__main__':
+    MyApp().run()
