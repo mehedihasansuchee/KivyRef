f15013e10c25d45c3b20a32dfa94aa7760f1e202
mehedihasansuchee <145920667+mehedihasansuchee@users.noreply.github.com>
2023-10-15 21:24:34 +0600

    Image1.py

---
+++ Image1.py
@@ -0,0 +1,12 @@
+from kivy.app import App 
+from kivy.uix.image import Image
+
+class MyImageApp(App):
+    def build(self):
+
+        image = Image(source='../img/ball.jpg')
+
+        return image
+    
+if __name__ == '__main__':
+    MyImageApp().run()
